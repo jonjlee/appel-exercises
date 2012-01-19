296af6c4f22ab61ce5f896d1d2a9e75c7feb7858
Jonathan Lee <jonjlee@gmail.com>
2012-01-19 15:59:03 -0500

    Implementation of maxargs()

--- src/main/java/appel/ch01/Interp.java
+++ src/main/java/appel/ch01/Interp.java
@@ -2,8 +2,59 @@ package appel.ch01;
 
 public class Interp {
 
-  public static int maxargs(Stm s) {
-    return 1;
+  /** Return number of expressions in an ExpList */
+  public static int numargs(ExpList exps) {
+    if (exps instanceof LastExpList) {
+      return 1;
+    }
+    return 1 + numargs(((PairExpList) exps).tail);
   }
 
+  /** Helper method to creates an ExpList from the arg list */ 
+  public static ExpList explist(Exp firstArg, Exp... otherArgs) {
+    if (otherArgs.length == 0) {
+      return new LastExpList(firstArg);
+    }
+    ExpList tail = new LastExpList(otherArgs[otherArgs.length-1]);
+    for (int i = otherArgs.length-2; i >= 0; i--) {
+      tail = new PairExpList(otherArgs[i], tail);
+    }
+    return new PairExpList(firstArg, tail);
+  }
+
+  /** Return the maximum number of args provided to any print statement occurring in s */ 
+  public static int maxargs(Stm s) {
+    if (s instanceof PrintStm) {
+      PrintStm printStm = (PrintStm) s;
+      return Math.max(numargs(printStm.exps), maxargs(printStm.exps));
+    } else if (s instanceof CompoundStm) {
+      CompoundStm cs = (CompoundStm) s;
+      return Math.max(maxargs(cs.stm1), maxargs(cs.stm2));
+    } else if (s instanceof AssignStm) {
+      return maxargs(((AssignStm) s).exp);
+    }
+    return 0;
+  }
+
+  /** maxarg helper for recursing into ExpLists, which can contain Exp objects that contain statements */
+  private static int maxargs(ExpList exps) {
+    if (exps instanceof LastExpList) {
+      return maxargs(((LastExpList) exps).head);
+    }
+    PairExpList pairExpList = (PairExpList) exps;
+    return Math.max(maxargs(pairExpList.head), maxargs(pairExpList.tail));
+  }
+  
+  /** maxarg helper for recursing into Exp objects, two subclasses of which can contain statements */
+  private static int maxargs(Exp e) {
+    if (e instanceof EseqExp) {
+      return maxargs(((EseqExp) e).stm);
+    } else if (e instanceof OpExp) {
+      OpExp opExp = (OpExp) e;
+      return Math.max(maxargs(opExp.left), maxargs(opExp.right));
+    }
+    return 0;
+  }
+  
+  
 }
--- src/test/java/appel/ch01/TestInterp.java
+++ src/test/java/appel/ch01/TestInterp.java
@@ -2,12 +2,69 @@ package appel.ch01;
 
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
+import static appel.ch01.Interp.*;
 
 public class TestInterp {
 
-  @Test public void maxargsHandlesSimplePrintStatement() {
-    Stm s = new PrintStm(new LastExpList(new NumExp(0)));
-    assertEquals(Interp.maxargs(s), 1);
+  private NumExp zero = new NumExp(0);
+  private PrintStm simplePrintStm = print(zero);
+  static Stm prog = 
+      new CompoundStm(new AssignStm("a", new OpExp(new NumExp(5), OpExp.Plus, new NumExp(3))),
+      new CompoundStm(new AssignStm("b", new EseqExp(
+                          new PrintStm(new PairExpList(new IdExp("a"), new LastExpList(new OpExp(new IdExp("a"), OpExp.Minus, new NumExp(1))))),
+                          new OpExp(new NumExp(10), OpExp.Times, new IdExp("a"))
+                      )),
+                      new PrintStm(new LastExpList(new IdExp("b")))));
+  
+  // numargs() tests
+  @Test public void numargsWithOneArg() {
+    assertEquals(numargs(new LastExpList(zero)), 1);
+  }
+  @Test public void numargsWithMultipleArgs() {
+    assertEquals(numargs(new PairExpList(zero, new PairExpList(zero, new LastExpList(zero)))), 3);
+  }
+
+  // explist() tests
+  @Test public void explistSingleElementList() {
+    assertEquals(numargs(explist(zero)), 1);
+  }
+  @Test public void explistMultiElementList() {
+    assertEquals(numargs(explist(zero, zero, zero)), 3);
+  }
+  @Test public void explistAcceptsArrayAsVarargs() {
+    assertEquals(numargs(explist(zero, new NumExp[] { zero, zero })), 3);
+  }
+
+  // maxargs() tests
+  @Test public void maxargsSimplePrintStatement() {
+    assertEquals(maxargs(simplePrintStm), 1);
+  }
+  @Test public void maxargsPrintStatementWithMultipleArgs() {
+    Stm s = print(zero, zero, zero);
+    assertEquals(maxargs(s), 3);
+  }
+  @Test public void maxargsCompoundStatement() {
+    Stm s = new CompoundStm(simplePrintStm, new AssignStm("x", zero));
+    assertEquals(maxargs(s), 1);
+  }
+  @Test public void maxargsESequence() {
+    Stm s = print(new EseqExp(print(zero, zero), zero));
+    assertEquals(maxargs(s), 2);
+  }
+  @Test public void maxargsOpExp() {
+    Stm s = print(new OpExp(new EseqExp(print(zero, zero, zero), new IdExp("x")), 0, zero));
+    assertEquals(maxargs(s), 3);
+  }
+  @Test public void maxargsAssignStm() {
+    Stm s = new AssignStm("x", new EseqExp(print(zero, zero, zero), zero));
+    assertEquals(maxargs(s), 3);
+  }
+  @Test public void maxargsLongProgram() {
+    assertEquals(maxargs(prog), 2);
   }
   
+  
+  private PrintStm print(Exp head, Exp... tail) {
+    return new PrintStm(explist(head, tail));
+  }
 }
